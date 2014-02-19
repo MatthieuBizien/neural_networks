8913f825dec3a6f792737a7627f6ed0eb42e373d
BayemanLawin <lawin.bayeman@gmail.com>
2014-02-19 14:19:16 +0100

    parser_csv (modification du main)
    Création de classe et de fonction.
    Normalement tient compte de tous les cas!
    Juste les paramètres à initialiser.
    Les labels doivent être codés à partir de 1.

---
+++ parser_csv/CSVManager.h
@@ -0,0 +1,8 @@
+#include "CSVRow.h"
+
+
+std::vector<std::vector<double>> CreateX(int, int);
+std::vector<std::vector<int>> CreateY(int, int);
+
+std::vector<std::vector<double>> GetX(std::vector<std::vector<double>> &, int, int, int, CSVRow);
+std::vector<std::vector<int>> GetY(std::vector<std::vector<int>> &, int, int, int, CSVRow);
--- parser_csv/main.cpp
+++ parser_csv/main.cpp
@@ -1,96 +1,35 @@
-#include <iterator>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-//using namespace std;
+#include "CSVManager.h"
 
-
-
-class CSVRow
-{
-    public:
-        std::string const& operator[](std::size_t index) const
-        {
-            return m_data[index];
-        }
-        std::size_t size() const
-        {
-            return m_data.size();
-        }
-
-        void readNextRow(std::istream& str)
-        {
-            std::string         line;
-            std::getline(str,line);
-
-            std::stringstream   lineStream(line);
-            std::string         cell;
-
-            m_data.clear();
-            while(std::getline(lineStream,cell,','))
-            {
-                m_data.push_back(cell);
-            }
-        }
-    private:
-        std::vector<std::string>    m_data;
-};
-
+//lecture ligne par ligne du fichier
 std::istream& operator>>(std::istream& str,CSVRow& data)
 {
     data.readNextRow(str);
     return str;
-}   
-
+}
 
 int main()
 {
-    std::ifstream       file("W:\plop.csv");
+	//chargement du fichier
+    std::ifstream       file("G:\wine.csv");
 
     CSVRow              row;
-
-	std::vector<std::vector<double>> MatX(178);
 	
-	for (int i=0; i<178; i++)
-	{
-		MatX[i]=std::vector<double>(13);
+	//initialisation des paramètres du fichier avec ncol ne contenant pas la colonne de Y
+	int ncol=13;
+	int nrow=178;
+	int nlabel=3;
 
-	}
-
-	std::vector<std::vector<int>> MatY(178);
-	for (int i=0; i<178; i++)
-	{
-		MatY[i]=std::vector<int>(3);
-
-	}
-
+	//Création des matrices X et Y
+	std::vector<std::vector<double>> X=CreateX(nrow,ncol);
+	std::vector<std::vector<int>> Y=CreateY(nrow,nlabel);
+	
+	//Remplissage des matrices X et Y
 	int i=0;
 
     while(file >> row)
     {
-		for (int j=0; j<13; j++)
-		{
-			double a=atof(row[j+1].c_str());
-			MatX[i][j]=a;
-		}
-
-		if (row[0]=="1")
-		{
-			MatY[i][0]=1;
-		}
-
-		if (row[0]=="2")
-		{
-			MatY[i][1]=1;
-		}
-
-		if (row[0]=="3")
-		{
-			MatY[i][2]=1;
-		}
+		GetX(X,i,nrow,ncol,row);
+		GetY(Y,i,nrow,nlabel,row);
 		i++;
-
 	}
 }
